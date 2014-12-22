521401e7443154ba2945c02730fe263cfae596f7
Anthony <anthshor@gmail.com>
2014-12-23 09:06:48 +1300

    first commit

---
+++ ulam.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#
+# Returns the first 25 ulam numbers. 
+#
+
+ulam = [1,2]
+test = Hash.new(0)
+counter = 0
+
+y = 2
+for i in 1..25
+    for x in 0..y-2
+      result = ulam[x] + ulam[y-1]
+      test[result] += 1
+      if (test[result] == 1)
+         ulam << result
+      elsif (test[result] > 1)
+         test[result].times {ulam.delete(result)}
+      else
+        null 
+      end
+    end
+  ulam.sort!
+  y += 1
+end
+puts "#{ulam[0..25]}"
